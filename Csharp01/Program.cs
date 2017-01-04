a88f9a48f1dd2d8ef0fcdb669085bfc9fdf580f8
robertomelo822 <robertomelo822@gmail.com>
2017-01-04 14:03:05 -0200

    Primeira Versao

--- Csharp01/Program.cs
+++ Csharp01/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Csharp01
 {
@@ -10,6 +6,9 @@ namespace Csharp01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+
+            Console.Read();
         }
     }
 }
